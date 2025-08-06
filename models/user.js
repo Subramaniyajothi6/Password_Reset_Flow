5f5b0be5215eb1699d517904a0fef50c5b7d3b27
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-08-06 15:56:26 +0530

    updated schema

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true, minLength: 6 },
-    // resetToken: String,
-    // resetTokenExpiration: Date,
+    resetToken: String,
+    resetTokenExpiration: Date,
 }, {
     timestamps: true
 })
